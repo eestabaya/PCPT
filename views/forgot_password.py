3815101da6ecd1d5daa5387d97789eabaa7c2bca
Eric Estabaya <eric@PapayaBook.local>
2020-11-22 21:43:16 -0800

    added comment for change

--- views/forgot_password.py
+++ views/forgot_password.py
@@ -3,7 +3,7 @@ from flask import Blueprint, render_template
 mod = Blueprint("view_password", __name__)
 
 
-@mod.route("/password")
+@mod.route("/password") # TODO this will probably need to change
 
 def view_forgot_password_page():
 
