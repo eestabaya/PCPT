84aec4df036fece40415bf23ef1b46c4ab1e62b3
Cory Wolterbeek <rwolterb@ucsd.edu>
2020-11-23 20:10:09 -0800

    working login page

--- views/login.py
+++ views/login.py
@@ -1,9 +1,13 @@
-from flask import render_template
-from PCPT import app
+from flask import render_template, Blueprint, flash, redirect
 from PCPT.views.forms import LoginForm
 # ...
+mod = Blueprint("login", __name__)
 
-@app.route('/login')
+@mod.route('/login', methods=['GET', 'POST '])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect('/index')
     return render_template('login.html', title='Sign In', form=form)
