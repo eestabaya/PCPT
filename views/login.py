a8614114666b0019a472a779eadfb6f0a1d4d091
nataliaucsd <nataliaucsd@github.com>
2020-11-27 07:24:30 -0800

    testing more login

--- views/login.py
+++ views/login.py
@@ -2,7 +2,10 @@ from flask import render_template, Blueprint, flash, redirect
 from .forms import LoginForm
 # ...
 mod = Blueprint("login", __name__)
-
+"""
+from flask_login import current_user, login_user
+from PCPT.models import User
+"""
 
 @mod.route('/login', methods=['GET', 'POST'])
 def login():
@@ -13,3 +16,18 @@ def login():
             form.username.data, form.remember_me.data))
         return redirect('/')
     return render_template('login.html', title='Sign In', form=form)
+
+"""
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
+"""
