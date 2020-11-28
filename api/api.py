00d65da7965b6bee5816efd03c8a14e427f20a16
Ian Lomibao <ilomibao@ucsd.edu>
2020-11-27 23:30:01 -0800

    adding a scraped product to the database
    skeleton is complete, but need to pass in the actual product and store data

--- api/api.py
+++ api/api.py
@@ -18,6 +18,53 @@ def get_from_mongo():
     except Exception as e:
         return {"success": False, "cause": str(e)}
 
+def add_scraped_product():
+    # TODO pass in product's ID, name, type, rating, and brand
+    product_id = 0
+    product_name = "test product"
+    product_type = "computer thing"
+    product_rating = 0.0
+    product_brand = "brand"
+
+    # TODO pass in store data
+    store_id = 0
+    product_price = 0.0
+    store_url = "https://store.com/product"
+
+    # product exists, update its Store field
+    if(db.product.find({"product_id":product_id}).count > 0):
+        db.product.update(
+            {"product_id":product_id},
+            {
+                "$push": {
+                    "stores": {
+                        "store_id": store_id,
+                        "product_price": product_price,
+                        "store_url": store_url
+                    }
+                }
+            }
+        )
+    # product does not exist, add it to the database
+    else:
+        db.product.insert(
+            {
+                "product_id": product_id,
+                "product_name": product_name,
+                "product_type": product_type,
+                "product_rating": product_rating,
+                "product_brand": product_brand,
+                "stores": [
+                    {
+                        "store_id": store_id,
+                        "product_price": product_price,
+                        "store_url": store_url
+                    }
+                ]
+            }
+        )
+    return {"success": True};
+
 
 @mod.route("/api/<uid>")
 def get_stuff(uid):
