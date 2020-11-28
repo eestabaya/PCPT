bbeb12fbb09b23c9be3aa7c21d55da428e9d87fa
nataliaucsd <nataliaucsd@github.com>
2020-11-28 08:40:23 -0800

    more copy paste for login,logout,registration

--- views/login.py
+++ views/login.py
@@ -3,8 +3,12 @@ from .forms import LoginForm
 # ...
 mod = Blueprint("login", __name__)
 """
+mod2 = Blueprint("logout", __name__)
 from flask_login import current_user, login_user
+from flask_login import logout_user
 from PCPT.models import User
+from app import db
+from app.forms import RegistrationForm
 """
 
 @mod.route('/login', methods=['GET', 'POST'])
@@ -30,4 +34,25 @@ def login():
         login_user(user, remember=form.remember_me.data)
         return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
+    
+@app.route('/logout')
+or could be
+@mod.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+    
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
 """
