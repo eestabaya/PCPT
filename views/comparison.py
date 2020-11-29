773ef14f7b79c57bd47dfc6b0e3fc1c8a5a5c295
fzieber <47437052+fzieber@users.noreply.github.com>
2020-11-28 19:38:27 -0800

    comparison.py skeleton
    Inspired by Eric's code for product.py. Placeholder for actual implementation later.

--- views/comparison.py
+++ views/comparison.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 mod = Blueprint("view_comparison", __name__)
 
@@ -6,7 +6,47 @@ mod = Blueprint("view_comparison", __name__)
 @mod.route("/comparison")
 
 def view_comparison_page():
+    # TODO for each item, item_id[i] = 'id'
+    item_id[0] = request.args.get('id')
+    print(request.args)
 
-    # TODO comparison logic
+    # TODO return an error page if header is incomplete
+    if item_id[0] is None:
+        return render_template("some_error_page_goes_here.html")
 
-    return render_template("comparison.html")
+    # do some database stuff
+    # get product from database using item id
+
+    # TODO return an error page if invalid product
+    item[0] = "something"
+    if item[0] is None:
+        return render_template("some_error_page_goes_here.html")
+
+    # then return product!
+    # placeholder below
+
+    hierarchy = {
+
+        # we found this from item_id
+        "item_1_id": {
+            "name": "",
+            "something": "else",
+
+            "websites": {
+
+                "amazon": {
+                    "link": "link here!",
+                    "price?": "idk what else"
+                },
+
+                "adorama": {
+                    "link": "link here!",
+                    "price?": "idk what else"
+                }
+            }
+        },
+
+        "item_2_id": {}
+    }
+
+    return render_template("comparison.html", var=hierarchy["item_1_id", "item_2_id"])
