0892a7c397ffb9b3460136cc460dea82e1c5631b
Colin Lemarchand <clemarch@ucsd.edu>
2020-12-12 23:28:10 -0800

    added product_specs to B&H scraping

--- utils/scrape.py
+++ utils/scrape.py
@@ -13,10 +13,13 @@ desktop_agents = [
     'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36',
     'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36',
     'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36',
-    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/602.2.14 (KHTML, like Gecko) Version/10.0.1 Safari/602.2.14',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/602.2.14 (KHTML, like Gecko) Version/10.0.1 '
+    'Safari/602.2.14',
     'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36',
-    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.98 Safari/537.36',
-    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.98 Safari/537.36',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.98 '
+    'Safari/537.36',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.98 '
+    'Safari/537.36',
     'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36',
     'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36',
     'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:50.0) Gecko/20100101 Firefox/50.0']
@@ -133,7 +136,16 @@ def extract_bh_page(source):
             for star in rating_section.contents:
                 if len(star.get('class')) == 3:
                     product_rating += 1
-        part_num_to_data[product_id] = [product_price, product_name, product_link, product_picture, product_rating]
+
+        product_specs_block = product_soup.find_all(
+            lambda tag: tag.name == 'li' and tag.get('data-selenium') == 'miniProductPageSellingPointsListItem')
+
+        product_specs = []
+        for product_spec in product_specs_block:
+            product_specs.append(product_spec.contents[0])
+
+        part_num_to_data[product_id] = \
+            [product_price, product_name, product_link, product_picture, product_rating, product_specs]
 
     return part_num_to_data
 
@@ -249,7 +261,7 @@ def get_gpu_prices():
     gpu_prices = {}
     # gpu_prices['adorama'] = scrape_adorama_category(store_urls['adorama']['gpu'])
     gpu_prices['bhphotovideo'] = scrape_bh_category(store_urls['bhphotovideo']['gpu'])
-    #gpu_prices['newegg'] = scrape_newegg_category(store_urls['newegg']['gpu'])
+    # gpu_prices['newegg'] = scrape_newegg_category(store_urls['newegg']['gpu'])
     return gpu_prices
 
 
@@ -257,5 +269,5 @@ def get_cpu_prices():
     cpu_prices = {}
     # cpu_prices['adorama'] = scrape_adorama_category(store_urls['adorama']['cpu'])
     cpu_prices['bhphotovideo'] = scrape_bh_category(store_urls['bhphotovideo']['cpu'])
-    #cpu_prices['newegg'] = scrape_newegg_category(store_urls['newegg']['cpu'])
+    # cpu_prices['newegg'] = scrape_newegg_category(store_urls['newegg']['cpu'])
     return cpu_prices
