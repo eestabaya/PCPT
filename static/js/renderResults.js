e8e5539da06149b78e12ccc790c37f1d9f549ad4
eestabaya <eestabay@ucsd.edu>
2020-12-13 03:25:32 -0800

    clean up scripts

--- static/js/renderResults.js
+++ static/js/renderResults.js
@@ -9,6 +9,22 @@ $(document).ready(function () {
     });
 });
 
+function starSort(rate, prod) {
+    let base = prod + "star";
+    switch (rate) {
+        case "5":
+            $(base + '5').toggleClass('blue');
+        case "4":
+            $(base + '4').toggleClass('blue');
+        case "3":
+            $(base + '3').toggleClass('blue');
+        case "2":
+            $(base + '2').toggleClass('blue');
+        case "1":
+            $(base + '1').toggleClass('blue');
+    }
+}
+
 /*
 $(document).ready(function () {
     $('.products').show();
