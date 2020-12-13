2d84200bd3e5dbd77cd0f1de9f25e077cb63b29f
eestabaya <eestabay@ucsd.edu>
2020-12-13 04:11:35 -0800

    quick update to remove extra data

--- views/home.py
+++ views/home.py
@@ -1,18 +1,13 @@
 from flask import Blueprint, render_template
-from flask_login import current_user, login_required
+from flask_login import current_user
 
 mod = Blueprint("home", __name__)
 
 
 @mod.route("/")
 def home():
-    stuff = {
-        "x": 23,
-        "y": "string",
-        "z": ["this", "is", "a", "list"]
-    }
 
     if current_user.is_authenticated:
-        return render_template("index.html", var=stuff, user=current_user)
+        return render_template("index.html", user=current_user)
 
-    return render_template("index.html", var=stuff)
+    return render_template("index.html")
